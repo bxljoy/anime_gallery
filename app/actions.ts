08a36179a38873a945ecf6a08f1cc3a92d6b1ff8
bxljoy <259500313@qq.com>
2024-06-17 02:06:56 +0200

    chore: Update AnimeCard image source URL to use absolute path

---
+++ app/actions.ts
@@ -0,0 +1,14 @@
+"use server";
+
+import AnimeCard, { AnimeProp } from "@/components/AnimeCard";
+
+const MAX_LIMIT = 8;
+
+export async function fetchAnime(page: number) {
+  const response = await fetch(
+    `https://shikimori.one/api/animes?page=${page}&limit=${MAX_LIMIT}&order=popularity`
+  );
+
+  const data = await response.json();
+  return data;
+}
